973f809955dba1e8d1209662fcde36456646de11
jp9141joao <jp9141joao@gmail.com>
2025-01-04 19:16:47 -0300

    commit

--- src/controller.ts
+++ src/controller.ts
@@ -6,6 +6,8 @@ import dotenv from 'dotenv';
 import { CreateUser, LoginUser, NewPasswordUser } from './request';
 import { Utils } from './utils';
 import { HttpResult } from './models/httpresult';
+import { decode } from 'punycode';
+import { Console } from 'console';
 
 dotenv.config();
 const SECRET_KEY = process.env.SECRET_KEY;
@@ -19,40 +21,34 @@ export const authSession = async (req: Request, res: Response): Promise<void> =>
             throw new Error("SECRET_KEY is not defined in the .env file.");
         }
 
-        if (!Utils.isValidEmail(email))  {
-            res.status(400).json(HttpResult.Fail("Error: The value of email is invalid!"));
+        if (!Utils.doesValueExist(email) || !Utils.isValidEmail(email))  {
+            res.status(404).json(HttpResult.Fail("Error: The value of email is invalid!"));
             return;
-        } else if (email.length > 255) {
-            res.status(400).json(HttpResult.Fail("Error: The value of email is too large!"));
-            return;;
         }
 
-        if (!Utils.isValidPassword(password)) {
-            res.status(400).json(HttpResult.Fail("Error: The value of password is invalid!"));
+        if (!Utils.doesValueExist(password) || !Utils.isValidPassword(password)) {
+            res.status(404).json(HttpResult.Fail("Error: The value of password is invalid!"));
             return;
-        } else if (password.length > 255) {
-            res.status(400).json(HttpResult.Fail("Error: The value of password is too large!"));
-            return;
-        } 
+        }
 
         const userData = await prisma.tb_user.findUnique({
             where: {
                 email: email
             }
-        })
+        })     
 
         if (!userData) {
-            res.status(400).json(HttpResult.Fail("Error: The user was not found!"));
+            res.status(400).json(HttpResult.Fail("Error: The email or password you entered is incorrect!"));
             return;
         }
 
         const validPassword = await bcrypt.compare(password, userData.password);
 
         if (!validPassword) {
-            res.status(400).json(HttpResult.Fail("Error: Invalid credentials!"));
+            res.status(404).json(HttpResult.Fail("Error: The email or password you entered is incorrect!"));
             return;
         }
-
+        
         const token = jwt.sign(
             { email },
             SECRET_KEY,
@@ -60,8 +56,10 @@ export const authSession = async (req: Request, res: Response): Promise<void> =>
         )
 
         res.status(200).json(HttpResult.Success(token));
+        console.log(token)
     } catch(error: any) {
         res.status(400).json(HttpResult.Fail("A unexpected error occured on authSession"));
+        console.error(error)
     }
 }
 
@@ -192,3 +190,47 @@ export const changePassword = async (req: Request, res: Response): Promise<void>
     }
 }
 
+export const getUser = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const authHeader = req.headers['authorization'];
+        const token = authHeader && authHeader.split(' ')[1];
+
+        if (!SECRET_KEY) {
+            throw new Error("SECRET_KEY is not defined in the .env file.");
+        }
+
+        if (!token) {
+            res.status(401).json(HttpResult.Fail("Token was not provided!"));
+            return;
+        }
+        
+        const decoded = jwt.verify(token, SECRET_KEY) as { email: string };
+        const email = decoded.email;
+
+        if (email) {
+            res.status(401).json(HttpResult.Fail("Token provided is invalid!"));
+            return;
+        }
+
+        const doesEmailExist = await prisma.tb_user.count({
+            where: {
+                email: email,
+            }
+        }) > 0 ? true : false;
+
+        if (!doesEmailExist) {
+            res.status(401).json(HttpResult.Fail("Error: Email does not exist!"));
+            return;
+        }
+
+        const gotUser = await prisma.tb_user.findUnique({
+            where: {
+                email: email,
+            }
+        });
+
+        res.status(200).json(HttpResult.Success({gotUser}));
+    } catch (error: any) {
+        res.status(400).json(HttpResult.Fail("A unexpected error occured on getUser!"));
+    }
+}
